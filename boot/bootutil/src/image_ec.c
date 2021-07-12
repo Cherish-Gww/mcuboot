2f5d1a8e70f70a8097d30b3e6688ff3486cedae7
Michael Thomas <michael.thomas@renesas.com>
2021-07-12 04:50:21 +0000

    added missing macro

--- boot/bootutil/src/image_ec.c
+++ boot/bootutil/src/image_ec.c
@@ -27,7 +27,7 @@
 
 #include "mcuboot_config/mcuboot_config.h"
 
-#if defined(MCUBOOT_SIGN_EC) || defined(MCUBOOT_SIGN_EC256)
+#if (defined(MCUBOOT_SIGN_EC) || defined(MCUBOOT_SIGN_EC256)) && (! defined(MCUBOOT_USE_TINYCRYPT) )
 #include "bootutil/sign_key.h"
 
 #include "mbedtls/ecdsa.h"
